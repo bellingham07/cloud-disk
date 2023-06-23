39e8cbbcd0bc143ea581ebb4aa5aba4e67854912
bellingham <75364055@qq.com>
2023-06-23 10:39:41 +0800

    feat:user file create😘

--- core/internal/logic/user_file_name_update_logic.go
+++ core/internal/logic/user_file_name_update_logic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"cloud-disk/core/models"
 	"context"
+	"errors"
 
 	"cloud-disk/core/internal/svc"
 	"cloud-disk/core/internal/types"
@@ -25,6 +26,15 @@ func NewUserFileNameUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 }
 
 func (l *UserFileNameUpdateLogic) UserFileNameUpdate(req *types.UserFileNameUpdateRequest, userIdentity string) (resp *types.UserFileNameUpdateReply, err error) {
+	// 判断当前名称是否已经在该目录下存在
+	count, err := l.svcCtx.Engine.Where("name=? AND parent_id = (SELECT parent_id FROM user_repository ur WHERE ur.identity=?)", req.Name, req.Identity).Count(new(models.UserRepository))
+	if err != nil {
+		return nil, err
+	}
+	if count > 0 {
+		return nil, errors.New("该名称已存在")
+	}
+	// 直接进行文件修改
 	data := &models.UserRepository{Name: req.Name}
 	_, err = l.svcCtx.Engine.Where("identity = ? AND user_identity = ?", req.Identity, userIdentity).Update(data)
 	if err != nil {
